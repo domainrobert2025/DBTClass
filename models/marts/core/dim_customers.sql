4f803bfdf2d60ef329d1747f0eaac7b2c8e7b855
Namrata L <domainrobert2025@gmail.com>
2026-09-03 02:36:43 +0000

    Dim updated

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -5,37 +5,31 @@
 }}
 
 with customers as (
-    select * from {{ source('jaffle_shop', 'customers') }}
+      select * from {{source('jaffle_shop','customers')}}
+), orders as (
+     select * from {{ source('jaffle_shop','orders') }}
 ),
-orders as (
-    select * from {{ source('jaffle_shop', 'orders') }}
-),
-payments as (
-    select * from {{ source('stripe', 'payment') }}
-),
+
 customer_orders as (
     select
-        o.user_id as customer_id,
-        min(o.order_date) as first_order_date,
-        max(o.order_date) as most_recent_order_date,
-        count(o.id) as number_of_orders,
-        sum(p.amount) as lifetime_value
-    from orders o
-    left join payments p
-        on o.id = p.order_id
-    group by o.user_id
+        id as customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(id) as number_of_orders,
+        sum(amount) as lifetime_value
+    from orders
+    group by 1
+),
+final as (
+    select
+       customers.customer_id,
+       customers.first_name,
+       customers.last_name,
+       customer_orders.first_order_date,
+       customer_orders.most_recent_order_date,
+       coalesce(customer_orders.number_of_orders,0) as number_of_orders,
+       customers_orders.lifetime_value
+    from Customers
+    feft join customer_orders using (customer_id)   
 )
-
-select
-    c.id as customer_id,
-    c.first_name,
-    c.last_name,
-    co.first_order_date,
-    co.most_recent_order_date,
-    co.number_of_orders,
-    co.lifetime_value
-from customers c
-left join customer_orders co
-    on c.id = co.customer_id
-
 select * from final
