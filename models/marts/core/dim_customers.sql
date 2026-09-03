ccbd797eec1be821bb5489938e1001c8c452f019
Namrata L <domainrobert2025@gmail.com>
2026-09-03 02:26:39 +0000

    Updated customer dimension

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -10,18 +10,18 @@ with customers as (
      select * from {{ source('jaffle_shop','orders') }}
 ),
 
-Customer_orders as (
-    Select
-        customer_id,
+customer_orders as (
+    select
+        id as customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
         sum(amount) as lifetime_value
-    From orders
-    Group by 1
+    from orders
+    group by 1
 ),
-Final as (
-    Select
+final as (
+    select
        customers.customer_id,
        customers.first_name,
        customers.last_name,
@@ -29,7 +29,7 @@ Final as (
        customer_orders.most_recent_order_date,
        coalesce(customer_orders.number_of_orders,0) as number_of_orders,
        lifetime_value   
-    From Customers
-    Left join customer_orders using (customer_id)   
+    from Customers
+    feft join customer_orders using (customer_id)   
 )
-Select * from Final
+select * from final
