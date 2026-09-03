56d4cfce9c226926d5bd53840520e9e8f9445caf
Namrata L <domainrobert2025@gmail.com>
2026-09-03 02:31:17 +0000

    Updated Customer Dim

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -15,7 +15,7 @@ customer_orders as (
         id as customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders,
+        count(id) as number_of_orders,
         sum(amount) as lifetime_value
     from orders
     group by 1
