db14246eeaaf05afaed0142924f5a48be02cad5a
Namrata L <domainrobert2025@gmail.com>
2026-09-04 07:28:49 +0000

    updated dim

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -12,7 +12,7 @@ with customers as (
 
 customer_orders as (
     select
-        customer_id,
+        id as customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
@@ -28,9 +28,9 @@ final as (
        customer_orders.first_order_date,
        customer_orders.most_recent_order_date,
        coalesce(customer_orders.number_of_orders,0) as number_of_orders,
-       customers_orders.lifetime_value
+       lifetime_value
     from customers
-      left join customer_orders on customers.customer_id = customer_orders.customer_id
+      left join customer_orders using (customer_id)
    
 )
 select * from final
